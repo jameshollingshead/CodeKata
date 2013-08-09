b9981951c1ab9917f70c3cc9bb1b0048063a0829
James Hollingshead <git@jameshollingshead.com>
2013-08-09 11:33:47 -0400

    KataBankOCR-8-9-2013 setup
    Initial setup for KataBankOCR project
    
    * Created folders for src and docs
    * Added user story and features documents to docs folder
    * Added empty solution to src
    * Added code and test projects to solution
    * Added references to nunit and source project to the test project
    
    That should be everything the initial setup needs. Since this is my
    first time doing TDD, I may be wrong about that, but it matches up with
    what I read and the Nunit test runner isn't complaining.

---
+++ BankOCR-8-9-2013/README.txt
@@ -0,0 +1,5 @@
+This is my first attempt at TDD.
+
+It contains the first two user stories from the KataBankOCR
+
+Since this is my first time using TDD, I will probably do things in a non optimal manner, but we all have to start somewhere, right? =]
---
+++ BankOCR-8-9-2013/src/KataBankOCR/KataBankOCRTest/KataTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace KataBankOCRTest
+{
+    class KataTest
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
