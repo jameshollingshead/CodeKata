5c17b076106e4206ca330f28c82ac951caa643b1
James Hollingshead <git@jameshollingshead.com>
2013-08-12 15:53:50 -0400

    Turn file into a file dump
    Added code to turn file into a file dump of string[]
    
    Added code to turn file dump into digital account numbers

--- BankOCR-8-9-2013/src/KataBankOCR/KataBankOCRTest/KataTest.cs
+++ BankOCR-8-9-2013/src/KataBankOCR/KataBankOCRTest/KataTest.cs
@@ -17,6 +17,18 @@ namespace KataBankOCR.UnitTest
         private OcrConstants ocrConstants = new OcrConstants();
         private string[][] _ocrAccountNumberAllZeros = new string[9][];
 
+        private string[] _dummyFileInput = 
+            {
+                " _  _  _  _  _  _  _  _  _ ",
+                "| || || || || || || || || |",
+                "|_||_||_||_||_||_||_||_||_|",
+                "                           ",
+                "                           ",
+                "  |  |  |  |  |  |  |  |  |",
+                "  |  |  |  |  |  |  |  |  |",
+                "                           "
+            };
+
         [SetUp]
         public void SetupUnitTests()
         {
@@ -35,7 +47,7 @@ namespace KataBankOCR.UnitTest
             var ocr1 = ocrConstants.Ocr1;
             var actualResult = _kataCode.ConvertOCRtoDigit(ocr1);
             var expectedResult = '1';
-            Assert.AreEqual(actualResult, expectedResult);
+            Assert.AreEqual(expectedResult, actualResult);
         }
 
         [Test]
@@ -44,7 +56,7 @@ namespace KataBankOCR.UnitTest
             var ocr2 = ocrConstants.Ocr2;
             var actualResult = _kataCode.ConvertOCRtoDigit(ocr2);
             var expectedResult = '2';
-            Assert.AreEqual(actualResult, expectedResult);
+            Assert.AreEqual(expectedResult, actualResult);
         }
 
         [Test]
@@ -53,7 +65,7 @@ namespace KataBankOCR.UnitTest
             var ocr3 = ocrConstants.Ocr3;
             var actualResult = _kataCode.ConvertOCRtoDigit(ocr3);
             var expectedResult = '3';
-            Assert.AreEqual(actualResult, expectedResult);
+            Assert.AreEqual(expectedResult, actualResult);
         }
 
         [Test]
@@ -62,7 +74,7 @@ namespace KataBankOCR.UnitTest
             var ocr4 = ocrConstants.Ocr4;
             var actualResult = _kataCode.ConvertOCRtoDigit(ocr4);
             var expectedResult = '4';
-            Assert.AreEqual(actualResult, expectedResult);
+            Assert.AreEqual(expectedResult, actualResult);
         }
 
         [Test]
@@ -71,7 +83,7 @@ namespace KataBankOCR.UnitTest
             var ocr5 = ocrConstants.Ocr5;
             var actualResult = _kataCode.ConvertOCRtoDigit(ocr5);
             var expectedResult = '5';
-            Assert.AreEqual(actualResult, expectedResult);
+            Assert.AreEqual(expectedResult, actualResult);
         }
 
         [Test]
@@ -80,7 +92,7 @@ namespace KataBankOCR.UnitTest
             var ocr6 = ocrConstants.Ocr6;
             var actualResult = _kataCode.ConvertOCRtoDigit(ocr6);
             var expectedResult = '6';
-            Assert.AreEqual(actualResult, expectedResult);
+            Assert.AreEqual(expectedResult, actualResult);
         }
 
         [Test]
@@ -89,7 +101,7 @@ namespace KataBankOCR.UnitTest
             var ocr7 = ocrConstants.Ocr7;
             var actualResult = _kataCode.ConvertOCRtoDigit(ocr7);
             var expectedResult = '7';
-            Assert.AreEqual(actualResult, expectedResult);
+            Assert.AreEqual(expectedResult, actualResult);
         }
 
         [Test]
@@ -98,7 +110,7 @@ namespace KataBankOCR.UnitTest
             var ocr8 = ocrConstants.Ocr8;
             var actualResult = _kataCode.ConvertOCRtoDigit(ocr8);
             var expectedResult = '8';
-            Assert.AreEqual(actualResult, expectedResult);
+            Assert.AreEqual(expectedResult, actualResult);
         }
 
         [Test]
@@ -107,7 +119,7 @@ namespace KataBankOCR.UnitTest
             var ocr9 = ocrConstants.Ocr9;
             var actualResult = _kataCode.ConvertOCRtoDigit(ocr9);
             var expectedResult = '9';
-            Assert.AreEqual(actualResult, expectedResult);
+            Assert.AreEqual(expectedResult, actualResult);
         }
 
         [Test]
@@ -116,7 +128,7 @@ namespace KataBankOCR.UnitTest
             var ocr0 = ocrConstants.Ocr0;
             var actualResult = _kataCode.ConvertOCRtoDigit(ocr0);
             var expectedResult = '0';
-            Assert.AreEqual(actualResult, expectedResult);
+            Assert.AreEqual(expectedResult, actualResult);
         }
 
         [Test]
@@ -124,7 +136,7 @@ namespace KataBankOCR.UnitTest
         {
             var actualResult = _kataCode.ConvertOcrAccountNumberToDigitAccountNumber(_ocrAccountNumberAllZeros);
             var expectedResult = "000000000";
-            Assert.AreEqual(actualResult, expectedResult);
+            Assert.AreEqual(expectedResult, actualResult);
         }
 
         [Test]
@@ -139,24 +151,25 @@ namespace KataBankOCR.UnitTest
 
             var actualResult = _kataCode.OcrAccountStringToDigitalAccountNumber(testInput);
             var expectedResult = "000000000";
-            Assert.AreEqual(actualResult, expectedResult);
+            Assert.AreEqual(expectedResult, actualResult);
         }
 
         [Test]
-        public void FourLineAccountNumberFileWithAllZeroOcrAccountNumberBecomesStringOfZeros()
+        public void EightLineAccountNumberFileWithAllZeroOcrAccountNumberBecomesStringOfZeros()
         {
-            string[] testInput = 
-            {
-                " _  _  _  _  _  _  _  _  _ ",
-                "| || || || || || || || || |",
-                "|_||_||_||_||_||_||_||_||_|",
-                "                           "
-            };
-
-            var actualResult = _kataCode.TurnFileContentsIntoDigitalAccountNumbers(testInput);
+            var actualResult = _kataCode.TurnFileContentsIntoDigitalAccountNumbers(_dummyFileInput);
             var expectedResult = "000000000";
-            Assert.AreEqual(actualResult[0], expectedResult);
-
+            Assert.AreEqual(expectedResult, actualResult[0]);
         }
+
+        [Test]
+        public void ReadInputFromFileAndReturnAccountNumberOfAllZeros()
+        {
+            string dummyFileName = "testfile.txt";
+            var actualResult = _kataCode.TurnFileIntoDigitalAccountNumbers(dummyFileName);
+            var expectedResult = "000000000";
+            Assert.AreEqual(expectedResult, actualResult[0]);
+        }
+        
     }
 }
