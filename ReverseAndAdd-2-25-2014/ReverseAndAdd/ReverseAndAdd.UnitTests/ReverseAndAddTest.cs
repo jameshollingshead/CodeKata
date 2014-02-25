91f5e3b5baf9bb18d033f0d0e9911ff1ead662ff
James Hollingshead <git@jameshollingshead.com>
2014-02-25 16:39:27 -0500

    ReverseAndAdd - Added 2 functions and tests
    Added ReverseNumberAsString
    Added AddTwoNumberStrings

--- ReverseAndAdd-2-25-2014/ReverseAndAdd/ReverseAndAdd.UnitTests/ReverseAndAddTest.cs
+++ ReverseAndAdd-2-25-2014/ReverseAndAdd/ReverseAndAdd.UnitTests/ReverseAndAddTest.cs
@@ -11,5 +11,42 @@ namespace ReverseAndAdd.UnitTests
     [TestFixture]
     public class ReverseAndAddTest
     {
+        public ReverseAndAdd _reverseAndAdd;
+
+        [SetUp]
+        public void SetUpUnitTests()
+        {
+            _reverseAndAdd = new ReverseAndAdd();
+        }
+
+        [Test]
+        public void Reverse123Gives321()
+        {
+            var forward = "123";
+            var expectedResult = "321";
+            var actualResult = _reverseAndAdd.ReverseNumberAsString(forward);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void Reverse957Gives759()
+        {
+            var forward = "957";
+            var expectedResult = "759";
+            var actualResult = _reverseAndAdd.ReverseNumberAsString(forward);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void Adding123And321Gives444()
+        {
+            var forward = "123";
+            var backward = "321";
+            var expectedResult = "444";
+            var actualResult = _reverseAndAdd.AddTwoNumberStrings(forward, backward);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
     }
+
+    
 }
--- ReverseAndAdd-2-25-2014/ReverseAndAdd/ReverseAndAdd/ReverseAndAdd.cs
+++ ReverseAndAdd-2-25-2014/ReverseAndAdd/ReverseAndAdd/ReverseAndAdd.cs
@@ -8,5 +8,24 @@ namespace ReverseAndAdd
 {
     public class ReverseAndAdd
     {
+        public string ReverseNumberAsString(string input)
+        {
+            string forward = input;
+            char[] charArray = forward.ToCharArray();
+            Array.Reverse(charArray);
+            string backward = new String(charArray);
+                        
+            return backward;
+        }
+
+        public string AddTwoNumberStrings(string forward, string backward)
+        {
+            int forwardAsInt = Convert.ToInt32(forward);
+            int backwardAsInt = Convert.ToInt32(backward);
+            int result = forwardAsInt + backwardAsInt;
+
+            string resultAsString = result.ToString();
+            return resultAsString;
+       }
     }
 }
