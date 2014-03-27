46583b5b2a8af5b8c950025e0c8cff4b82e77b1b
James Hollingshead <git@jameshollingshead.com>
2014-03-27 12:19:07 -0400

    PredictTheNumber - Added DetermineNeededStringLength Added a metric boatlaod of unit tests to cover standard and edge cases. All tests green.

--- PredictTheNumber-3-27-2014/ConsoleApplication1/ConsoleApplication1/PredictTheNumber.cs
+++ PredictTheNumber-3-27-2014/ConsoleApplication1/ConsoleApplication1/PredictTheNumber.cs
@@ -8,7 +8,28 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("PredictTheNumber.Tests")]
 namespace PredictTheNumber
 {
-    internal class PredictTheNumber
+    public class PredictTheNumber
     {
+        internal long DetermineNeededStringLength(int targetPosition)
+        {
+            long result;
+            int powerOfTwo;
+
+            //Since Log2(0) = infinity, we have to treat this as an edge case whose answer is 1
+            if (targetPosition == 0)
+            {
+                result = 1;
+            }
+            else
+            {
+                //adjust target position by 1 so we can use "normal people" (counting numbers start at 1) math.
+                targetPosition += 1;
+
+                powerOfTwo = (int)Math.Ceiling(Math.Log(targetPosition, 2));
+                result = (long)Math.Pow(2, powerOfTwo);
+            }
+
+            return result;
+        }
     }
 }
