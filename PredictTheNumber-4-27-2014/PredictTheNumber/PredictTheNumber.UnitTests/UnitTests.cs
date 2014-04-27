8121fecf9483cd40f28a1ba0f5290adfe5b82041
James Hollingshead <git@jameshollingshead.com>
2014-04-27 17:36:53 -0400

    PredictTheNumber-4-27-2014 - Added GetCompletedNumberString Added PerformTransformOnNumberString Wrote associated unit tests All tests green

--- PredictTheNumber-4-27-2014/PredictTheNumber/PredictTheNumber.UnitTests/UnitTests.cs
+++ PredictTheNumber-4-27-2014/PredictTheNumber/PredictTheNumber.UnitTests/UnitTests.cs
@@ -11,11 +11,82 @@ namespace PredictTheNumber.Tests
     [TestFixture]
     public class UnitTests
     {
+        public PredictTheNumber predictTheNumber;
+
         [SetUp]
         public void TestSetup()
         {
-
+            predictTheNumber = new PredictTheNumber();
         }
 
+        [Test]
+        public void String0ShouldReturn01()
+        {
+            string input = "0";
+            string expectedResult = "01";
+            string actualResult = predictTheNumber.PerformTransformOnNumberString(input);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void String01ShouldReturn0112()
+        {
+            string input = "01";
+            string expectedResult = "0112";
+            string actualResult = predictTheNumber.PerformTransformOnNumberString(input);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void String0112ShouldReturn01121220()
+        {
+            string input = "0112";
+            string expectedResult = "01121220";
+            string actualResult = predictTheNumber.PerformTransformOnNumberString(input);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void Position0ShouldReturn0()
+        {
+            long targetPosition = 0;
+            string expectedResult = "0";
+            string actualResult = predictTheNumber.GetCompletedNumberString(targetPosition);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        
+        [Test]
+        public void Position1ShouldReturn01()
+        {
+            long targetPosition = 1;
+            string expectedResult = "01";
+            string actualResult = predictTheNumber.GetCompletedNumberString(targetPosition);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void Position3ShouldReturn0112()
+        {
+            long targetPosition = 3;
+            string expectedResult = "0112";
+            string actualResult = predictTheNumber.GetCompletedNumberString(targetPosition);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void Position5ShouldReturn01121220()
+        {
+            long targetPosition = 5;
+            string expectedResult = "01121220";
+            string actualResult = predictTheNumber.GetCompletedNumberString(targetPosition);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
     }
 }
--- PredictTheNumber-4-27-2014/PredictTheNumber/PredictTheNumber/PredictTheNumber.cs
+++ PredictTheNumber-4-27-2014/PredictTheNumber/PredictTheNumber/PredictTheNumber.cs
@@ -6,10 +6,49 @@ using System.Threading.Tasks;
 using System.Runtime.CompilerServices;
 
 
-[assembly: InternalsVisibleTo("PredictTheNumber.Tests")]
+[assembly: InternalsVisibleTo("PredictTheNumber.UnitTests")]
 namespace PredictTheNumber
 {
-    class PredictTheNumber
+    public class PredictTheNumber
     {
+        internal string GetCompletedNumberString(long targetPosition)
+        {
+            string result;
+            long minimumStringLength = targetPosition + 1; //Counting is 0 based, so we need to correct for this
+            string numberString = "0";
+
+            while(numberString.Length < minimumStringLength)
+            {
+                numberString = PerformTransformOnNumberString(numberString);
+            }
+
+            result = numberString;
+            
+            return result;
+        }
+
+        internal string PerformTransformOnNumberString(string input)
+        {
+            string result = input;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if(input[i] == '0')
+                {
+                    result += "1";
+                }
+                if (input[i] == '1')
+                {
+                    result += "2";
+                }
+                if (input[i] == '2')
+                {
+                    result += "0";
+                }
+
+            }
+
+                return result;
+        }
     }
 }
