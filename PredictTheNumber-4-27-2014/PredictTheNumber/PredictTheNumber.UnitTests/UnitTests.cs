92846d6024a6af52ac32b01d699cb7e3122d4565
James Hollingshead <git@jameshollingshead.com>
2014-04-27 16:22:57 -0400

    Created PredictTheNumber-4-27-2014 Set up unit testing framework

---
+++ PredictTheNumber-4-27-2014/PredictTheNumber/PredictTheNumber.UnitTests/UnitTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using PredictTheNumber;
+
+namespace PredictTheNumber.Tests
+{
+    [TestFixture]
+    public class UnitTests
+    {
+        [SetUp]
+        public void TestSetup()
+        {
+
+        }
+
+    }
+}
