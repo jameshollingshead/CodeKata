55ebe4f7744152c8b0e36a79d34db7ca354cfb2a
James Hollingshead <git@jameshollingshead.com>
2014-10-02 18:20:13 -0400

    RomanNumerals - Refactor ConvertArabicToRomanNumerals
    No functional changes, but code is much easier to read now.

--- RomanNumerals-10-2-2014/RomanNumerals-10-2-2014/RomanNumerals-10-2-2014/RomanNumerals.cs
+++ RomanNumerals-10-2-2014/RomanNumerals-10-2-2014/RomanNumerals-10-2-2014/RomanNumerals.cs
@@ -12,6 +12,9 @@ namespace RomanNumerals_10_2_2014
         {
             string result = string.Empty;
             int numberToConvert = input;
+            string[] romanNumerals = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+            int[] arabicNumerals = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+            int conversionArrayLength = romanNumerals.Length;
 
             //This is basicall a sieve, taking the largest possible numeral on each pass.
             //It's not the most efficent way to do this, but given the small problem size
@@ -19,70 +22,16 @@ namespace RomanNumerals_10_2_2014
 
             while (numberToConvert > 0)
             {
-                if (numberToConvert >= 1000)
+                bool convertedNumber = false;
+
+                for(int i = 0; i < conversionArrayLength && convertedNumber == false; i++)
                 {
-                    result += "M";
-                    numberToConvert -= 1000;
-                }
-                else if (numberToConvert >= 900)
-                {
-                    result += "CM";
-                    numberToConvert -= 900;
-                }
-                else if (numberToConvert >= 500)
-                {
-                    result += "D";
-                    numberToConvert -= 500;
-                }
-                else if (numberToConvert >= 400)
-                {
-                    result += "CD";
-                    numberToConvert -= 400;
-                }
-                else if (numberToConvert >= 100)
-                {
-                    result += "C";
-                    numberToConvert -= 100;
-                }
-                else if (numberToConvert >= 90)
-                {
-                    result += "XC";
-                    numberToConvert -= 90;
-                }
-                else if (numberToConvert >= 50)
-                {
-                    result += "L";
-                    numberToConvert -= 50;
-                }
-                else if (numberToConvert >= 40)
-                {
-                    result += "XL";
-                    numberToConvert -= 40;
-                }
-                else if (numberToConvert >= 10)
-                {
-                    result += "X";
-                    numberToConvert -= 10;
-                }
-                else if(numberToConvert >= 9)
-                {
-                    result += "IX";
-                    numberToConvert -= 9;
-                }
-                else if(numberToConvert >= 5)
-                {
-                    result += "V";
-                    numberToConvert -= 5;
-                }
-                else if(numberToConvert >= 4)
-                {
-                    result += "IV";
-                    numberToConvert -= 4;
-                }
-                else
-                {
-                    result += "I";
-                    numberToConvert -= 1;
+                    if(numberToConvert >= arabicNumerals[i])
+                    {
+                        result += romanNumerals[i];
+                        numberToConvert -= arabicNumerals[i];
+                        convertedNumber = true;
+                    }
                 }
             }
 
