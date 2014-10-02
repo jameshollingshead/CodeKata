ca9b87bae985b08860c35797b053c03c6bf040d2
James Hollingshead <git@jameshollingshead.com>
2014-10-02 16:35:36 -0400

    RomanNumeral - Test framework set up

--- RomanNumerals-10-2-2014/RomanNumerals-10-2-2014/RomanNumerals-10-2-2014.Test/UnitTests.cs
+++ RomanNumerals-10-2-2014/RomanNumerals-10-2-2014/RomanNumerals-10-2-2014.Test/UnitTests.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
+using RomanNumerals_10_2_2014;
 
 namespace RomanNumerals_10_2_2014.Test
 {
-    public class Class1
+    [TestFixture]
+    public class UnitTests
     {
     }
 }
--- RomanNumerals-10-2-2014/RomanNumerals-10-2-2014/RomanNumerals-10-2-2014/RomanNumerals.cs
+++ RomanNumerals-10-2-2014/RomanNumerals-10-2-2014/RomanNumerals-10-2-2014/RomanNumerals.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace RomanNumerals_10_2_2014
 {
-    public class Class1
+    public class RomanNumerals
     {
     }
 }
