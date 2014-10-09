86cb7319335aad8024ab93ce475d27dcb761565f
James Hollingshead <git@jameshollingshead.com>
2014-10-09 11:50:50 -0400

    FibonacciSeries - created driver functions for project (read file, convert input, write output)

--- FibonacciSeries-10-9-2014/FibonacciSeries/FibonacciSeries/Program.cs
+++ FibonacciSeries-10-9-2014/FibonacciSeries/FibonacciSeries/Program.cs
@@ -10,6 +10,32 @@ namespace FibonacciSeries
     {
         static void Main(string[] args)
         {
+            string fileName = args[0];
+            string[] fileContents;
+            int numberInSeries;
+            int calculatedFibonacciNumber;
+            FibonacciNumber fibonacciNumber = new FibonacciNumber();
+
+            fileContents = GetFileContents(fileName);
+
+            for(int i = 0; i<fileContents.Length; i++)
+            {
+                numberInSeries = Convert.ToInt32(fileContents[i]);
+                calculatedFibonacciNumber = fibonacciNumber.CalculateFibonacciNumber(numberInSeries);
+                Console.WriteLine(calculatedFibonacciNumber);
+            }
+
+
+
+        }
+
+        private static string[] GetFileContents(string fileName)
+        {
+            string[] result;
+
+            result = System.IO.File.ReadAllLines(fileName);
+
+            return result;
         }
 
         public int CalculateFibonacciNumber(int input)
@@ -19,7 +45,6 @@ namespace FibonacciSeries
             int tmp;
             int result;
 
-            //Special Cases
             if (input == 0)
                 result = 0;
             else if (input == 1)
