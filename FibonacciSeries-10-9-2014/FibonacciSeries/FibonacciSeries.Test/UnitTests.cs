989a1dde9d48498f5aa420c3cb1496b88fd98df8
James Hollingshead <git@jameshollingshead.com>
2014-10-09 11:40:41 -0400

    FibonacciNumber - finonacci number generator tested through n=5. Given that it's a simple calculation, all output should be correct;

---
+++ FibonacciSeries-10-9-2014/FibonacciSeries/FibonacciSeries.Test/UnitTests.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using FibonacciSeries;
+
+namespace FibonacciSeries.Test
+{
+    [TestFixture]
+    public class UnitTests
+    {
+        public FibonacciNumber fibonacciNumber;
+
+        [SetUp]
+        public void TestSetup()
+        {
+            fibonacciNumber = new FibonacciNumber();
+        }
+
+        [Test]
+        public void Fib0is0()
+        {
+            int input = 0;
+            int expectedResult = 0;
+            int actualResult;
+
+            actualResult = fibonacciNumber.CalculateFibonacciNumber(input);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void Fib1is1()
+        {
+            int input = 1;
+            int expectedResult = 1;
+            int actualResult;
+
+            actualResult = fibonacciNumber.CalculateFibonacciNumber(input);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void Fib2is1()
+        {
+            int input = 2;
+            int expectedResult = 1;
+            int actualResult;
+
+            actualResult = fibonacciNumber.CalculateFibonacciNumber(input);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void Fib3is2()
+        {
+            int input = 3;
+            int expectedResult = 2;
+            int actualResult;
+
+            actualResult = fibonacciNumber.CalculateFibonacciNumber(input);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void Fib4is3()
+        {
+            int input = 4;
+            int expectedResult = 3;
+            int actualResult;
+
+            actualResult = fibonacciNumber.CalculateFibonacciNumber(input);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void Fib5is5()
+        {
+            int input = 5;
+            int expectedResult = 5;
+            int actualResult;
+
+            actualResult = fibonacciNumber.CalculateFibonacciNumber(input);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
---
+++ FibonacciSeries-10-9-2014/FibonacciSeries/FibonacciSeries/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciSeries
+{
+    public class FibonacciNumber
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public int CalculateFibonacciNumber(int input)
+        {
+            int previousFibNumber = 0;
+            int currentFibNumber = 1;
+            int tmp;
+            int result;
+
+            //Special Cases
+            if (input == 0)
+                result = 0;
+            else if (input == 1)
+                result = 1;
+            else
+            {
+                for (int i = 2; i <= input; i++)
+                {
+                    tmp = previousFibNumber + currentFibNumber;
+                    previousFibNumber = currentFibNumber;
+                    currentFibNumber = tmp;
+                }
+                result = currentFibNumber;
+            }
+
+            
+            return result;
+
+        }
+    }
+}
