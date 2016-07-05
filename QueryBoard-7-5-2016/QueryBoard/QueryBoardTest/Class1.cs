3aa80f6f16c2f578a6b6400c215fce70666ee493
James Hollingshead <git@jameshollingshead.com>
2016-07-05 16:16:08 -0400

    Added QueryBoard Kata
    New kata added.
    First test written and passed

---
+++ QueryBoard-7-5-2016/QueryBoard/QueryBoardTest/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit;
+using QueryBoard;
+
+namespace QueryBoard.Test
+{
+    [TestFixture]
+    public class UnitTests
+    {
+        public QueryBoard queryBoard;
+
+        [SetUp]
+        public void TestSetup()
+        {
+            queryBoard = new QueryBoard();
+        }
+
+        [Test]
+        public void CreateBoardShouldCreateANewBoard()
+        {
+            var expectedResult = new int[256, 256];
+            var actualResult = queryBoard.CreateBoard();
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
