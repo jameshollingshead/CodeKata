e0ef7c744408b291e2349abee5641f2c40268df9
James Hollingshead <git@jameshollingshead.com>
2017-07-24 17:47:32 -0400

    Insert Coin and Dime Inserted
    Vending machine now handles looking at the display for no coins inserted
    and a dime inserted

---
+++ VendingMachine-7-24-2017/VendingMachine/VendingMachine/VendingMachine.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachines
+{
+    public class VendingMachine
+    {
+        private double total = 0.0;
+
+        public String GetDisplay()
+        {
+            if (total == 0)
+            {
+                return "Insert Coin";
+            }
+            else
+            {
+                return "$0.10";
+            }
+        }
+
+        public void InsertCoin(string coin)
+        {
+            total = 0.10;
+        }
+    }
+}
---
+++ VendingMachine-7-24-2017/VendingMachine/VendingMachineTest/VendingMachineUnitTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using VendingMachines;
+
+
+namespace VendingMachineTest
+{
+    [TestFixture]
+    class VendingMachineUnitTests
+    {
+        public VendingMachine VendingMachine;
+        public const string InsertCoin = "Insert Coin";
+        public const string DimeInserted = "$0.10";
+
+        [SetUp]
+        public void TestSetup()
+        {
+            VendingMachine = new VendingMachine();
+        }
+
+        [Test]
+        public void LookingAtDisplayWithNoCoinsInsertedShouldSayInsertCoin()
+        {
+            var result = VendingMachine.GetDisplay();
+            Assert.AreEqual(InsertCoin, result);
+        }
+
+        [Test]
+        public void WhenADimeIsInsertedTheDisplayWillSayThereAre10Cents()
+        {
+            VendingMachine.InsertCoin("Dime");
+            var result = VendingMachine.GetDisplay();
+            Assert.AreEqual(DimeInserted, result);
+        }
+
+        
+    }
+}
